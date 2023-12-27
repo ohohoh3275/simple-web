1dd17a12fa64c12cf1c6e312c46351eea7f9df64
ohjiyeon3275 <ojy940716@gmail.com>
2023-12-27 20:55:17 +0900

    feat(card): simple usage of tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import axios from "axios";
-import { useEffect } from "react";
+import Card from "@/components/Card";
 
 export default function Home() {
-  let data;
-  useEffect(() => {
-    axios(`http://${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/v1/main`, {
-      method: "get",
-    }).then((res) => {
-      data = res;
-    });
-  }, []);
-  return <div>{data}</div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 }
---
+++ frontend/src/components/Card/index.tsx
@@ -0,0 +1,9 @@
+import "./index.styles.css";
+
+export default function Card() {
+  return (
+    <div className="w-3/12 bg-amber-300 h-80 border-2 border-stone-600	">
+      card
+    </div>
+  );
+}
