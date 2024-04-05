d1dfa27a7a6b7de53ca8786a51884f635ba2ab89
ohjiyeon3275 <ojy940716@gmail.com>
2024-04-05 12:40:37 +0900

    feat: percent base

--- frontend/src/client/pages/three.tsx
+++ frontend/src/client/pages/three.tsx
@@ -1,9 +1,24 @@
+"use client"
+
+import { useState } from 'react';
 import './three.css';
 
 export default function PageThree() {
 
+    const [percent, setPercent] = useState(0);
+
+    window.addEventListener("scroll", (e) => {
+        const scrollY = window.scrollY;
+        const innerHeight = document.body.scrollHeight - window.innerHeight;
+        const per = (scrollY / innerHeight) * 100
+        setPercent(per)
+    })
+
     return (
         <>
+        <div className="percent">
+            {percent.toFixed() + "%"}
+        </div>
         <div className="parallax"></div>
         <div
             style={{
@@ -16,7 +31,6 @@ export default function PageThree() {
         This div is just here to enable scrolling.
         Tip: Try to remove the background-attachment property to remove the scrolling effect.
         </div>
-
         <div className="parallax"></div>
         </>
     )
