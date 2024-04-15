047e6aaaee1793f38679be19688ff0bea436d516
ohjiyeon3275 <ojy940716@gmail.com>
2024-04-15 12:57:52 +0900

    feat: three added

--- frontend/src/app/four/page.tsx
+++ frontend/src/app/four/page.tsx
@@ -1,8 +1,9 @@
+import PageFour from "@/client/pages/four"
 import { useEffect } from "react"
 
 export default function Four() {
     
     return (
-        <>four</>
+        <PageFour/>
     )
 }
